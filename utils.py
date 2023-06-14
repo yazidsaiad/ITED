557815e9403087b8feef1725a8de4b125f542453
SAIAD Yazid (EXT TNP Consultants) <PYSD10221@commun.ad.sncf.fr>
2023-06-14 19:13:13 +0200

    deleted some requirements

--- utils.py
+++ utils.py
@@ -1,11 +1,11 @@
 import pandas as pd
-import base64
-import pickle
-import uuid
-import re
+# import base64
+# import pickle
+# import uuid
+# import re
 import streamlit as st
 # import os
-import json
+# import json
 
 
 
@@ -74,7 +74,7 @@ def material_series_string_generation(df_couples : pd.DataFrame, dict_output : d
                 s += (f"consist_pattern_unit;{single};\n")
     return s
     
-
+'''
 def download_link(object_to_download, download_filename, download_link_text):
     """
     Generates a link to download the given object_to_download.
@@ -178,6 +178,8 @@ def download_button(object_to_download, download_filename, button_text, pickle_i
     dl_link = custom_css + f'<a download="{download_filename}" id="{button_id}" href="data:file/txt;base64,{b64}">{button_text}</a><br></br>'
 
     return dl_link
+
+    '''
 
 def material_series_generation(df_couples : pd.DataFrame, dict_output : dict, CP :str):
 
